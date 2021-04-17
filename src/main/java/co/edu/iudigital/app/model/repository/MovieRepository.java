260153155214df723e187d56636fbba52f15629c
Jonathan Canadá <jonathan.acevedo@est.iudigital.edu.co>
2021-04-16 22:59:25 -0500

    first commit

---
+++ src/main/java/co/edu/iudigital/app/model/repository/MovieRepository.java
@@ -0,0 +1,6 @@
+package co.edu.iudigital.app.model.repository;
+import org.springframework.data.repository.CrudRepository;
+import co.edu.iudigital.app.model.entity.Movie;
+
+public interface MovieRepository extends CrudRepository<Movie, Integer>{
+}
---
+++ src/main/java/co/edu/iudigital/app/model/repository/ProfileRepository.java
@@ -0,0 +1,6 @@
+package co.edu.iudigital.app.model.repository;
+import org.springframework.data.repository.CrudRepository;
+import co.edu.iudigital.app.model.entity.Profile;
+
+public interface ProfileRepository extends CrudRepository<Profile, Integer>{
+}
